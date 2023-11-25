f6bbe4fdcf9af60341a41cc870e58f97cd75bf04
RomantsovS <romancov123@gmail.com>
2023-11-25 22:51:02 +0300

    3.1.materials

---
+++ src/2.lighting/3.1.materials/3.1.materials.fs
@@ -0,0 +1,46 @@
+#version 330 core
+out vec4 FragColor;
+
+uniform vec3 viewPos;
+
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+}; 
+  
+uniform Material material;
+
+struct Light {
+    vec3 position;
+  
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform Light light;
+
+in vec3 Normal;
+in vec3 FragPos;
+
+void main()
+{  	
+    // diffuse 
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    
+    // specular
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+
+    vec3 ambient  = light.ambient * material.ambient;
+    vec3 diffuse  = light.diffuse * (diff * material.diffuse);
+    vec3 specular = light.specular * (spec * material.specular); 
+        
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
+}
